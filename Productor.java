0d8e60d82a9c1762055fa0a6ca89aec1c15beed8
lauram354 <l.murciac@uniandes.edu.co>
2024-09-04 21:54:35 -0500

    creacion clases

---
+++ Productor.java
@@ -0,0 +1,12 @@
+public class Productor {
+    private int id; 
+    private String tipo;
+    private int numProductos;
+
+    public void Productor(int id, String tipo, int numProductos){
+        this.id = id;
+        this.tipo = tipo;
+        this.numProductos = numProductos;
+    }
+
+}
