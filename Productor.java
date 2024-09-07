06053d9bb8f3f008a7072dfa4e574fd0aa49b6eb
lauram354 <l.murciac@uniandes.edu.co>
2024-09-06 21:33:58 -0500

    definicion de aspectos basicos

--- Productor.java
+++ Productor.java
@@ -1,4 +1,4 @@
-public class Productor {
+public class Productor extends Thread{
     private int id; 
     private String tipo;
     private int numProductos;
@@ -9,4 +9,8 @@ public class Productor {
         this.numProductos = numProductos;
     }
 
+    @Override
+    public void run(){
+
+    }
 }
